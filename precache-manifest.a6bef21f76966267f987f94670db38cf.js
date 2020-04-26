5ef2853e815ae6d0cb4e191225289617405d0fb7
subratsri <subrat2406@gmail.com>
2020-04-26 06:08:39 +0530

    Updates

---
+++ precache-manifest.a6bef21f76966267f987f94670db38cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0ae87eaf0d46d0502cba2f112fc9e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c327453d711cd3b1e52",
+    "url": "/monsters-rolodex/static/css/main.2888a041.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c327453d711cd3b1e52",
+    "url": "/monsters-rolodex/static/js/main.f41664bf.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
